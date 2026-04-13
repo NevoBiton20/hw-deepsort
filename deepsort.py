bb8f92ecd3f8c33cd0b48ee4b71561c231854c50
Nevo Biton <155353129+NevoBiton20@users.noreply.github.com>
2026-04-13 14:27:58 +0300

    Update deepsort.py

--- deepsort.py
+++ deepsort.py
@@ -1,29 +1,16 @@
 def deep_sorted(x: any) -> str:
-    def sort_key(v):
-        try:
-            return (0, int(v))
-        except:
-            return (1, str(v))
-
-    def atom(v):
-        try:
-            return str(int(v))
-        except:
-            return str(v)
-
     if isinstance(x, dict):
-        items = sorted(x.items(), key=lambda kv: sort_key(kv[0]))
+        items = sorted(x.items(), key=lambda kv: str(kv[0]))
         return '{' + ', '.join(f'{deep_sorted(k)}: {deep_sorted(v)}' for k, v in items) + '}'
     if isinstance(x, list):
-        return '[' + ', '.join(deep_sorted(v) for v in sorted(x, key=sort_key)) + ']'
+        return '[' + ', '.join(deep_sorted(v) for v in sorted(x, key=str)) + ']'
     if isinstance(x, tuple):
-        s = ', '.join(deep_sorted(v) for v in sorted(x, key=sort_key))
+        s = ', '.join(deep_sorted(v) for v in sorted(x, key=str))
         return '(' + s + (',' if len(x) == 1 else '') + ')'
     if isinstance(x, set):
-        return '{' + ', '.join(deep_sorted(v) for v in sorted(x, key=sort_key)) + '}'
-    return atom(x)
+        return '{' + ', '.join(deep_sorted(v) for v in sorted(x, key=str)) + '}'
+    return str(x)
 
 
 if __name__ == '__main__':
-    import doctest
-    print(doctest.testmod())
+    print(deep_sorted([1, {'a': 2, 'b': (4, 3, [5, 2]), 'c': [9, 8]}, (7, 6), '10']))
