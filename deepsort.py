5978c620a7b01411e8a4873b4ca99f93e7747437
Nevo Biton <155353129+NevoBiton20@users.noreply.github.com>
2026-04-13 18:14:55 +0300

    Update deepsort.py

--- deepsort.py
+++ deepsort.py
@@ -1,33 +1,53 @@
-def deep_sorted(x: any) -> str:
-    def sort_key(v):
+def deep_sorted(x:any)->str:
+    if isinstance(x, str):
+        # Try to convert string to number
         try:
-            return (0, int(v))
-        except:
-            return (1, str(v))
-
-    def out(v):
-        if isinstance(v, str):
+            return int(x)
+        except ValueError:
             try:
-                return str(int(v))
-            except:
-                return repr(v)
-        return repr(v) if isinstance(v, str) else str(v)
+                return float(x)
+            except ValueError:
+                return x
+    elif isinstance(x, dict):
+        return {k: deep_sorted(x[k]) for k in sorted(x.keys())}
+    elif isinstance(x, list):
+        sorted_elements = [deep_sorted(k) for k in x]
+        return sorted(sorted_elements, key=lambda k: serialize(k))
+    elif isinstance(x, tuple):
+        sorted_elements = [deep_sorted(k) for k in x]
+        return tuple(sorted(sorted_elements, key=lambda k: serialize(k)))
+    elif isinstance(x, set):
+        sorted_elements = [deep_sorted(k) for k in x]
+        return set(sorted(sorted_elements, key=lambda k: serialize(k)))
+    else:
+        return x
 
-    if isinstance(x, dict):
-        items = sorted(x.items(), key=lambda kv: sort_key(kv[0]))
-        return '{' + ', '.join(f'{deep_sorted(k)}: {deep_sorted(v)}' for k, v in items) + '}'
-    if isinstance(x, list):
-        return '[' + ', '.join(deep_sorted(v) for v in sorted(x, key=sort_key)) + ']'
-    if isinstance(x, tuple):
-        s = ', '.join(deep_sorted(v) for v in sorted(x, key=sort_key))
-        return '(' + s + (',' if len(x) == 1 else '') + ')'
-    if isinstance(x, set):
-        return '{' + ', '.join(deep_sorted(v) for v in sorted(x, key=sort_key)) + '}'
-    return out(x)
 
+def serialize(obj):
+    if isinstance(obj, dict):
+        inner = ", ".join(f'"{k}": {serialize(v)}' for k, v in obj.items())
+        return "{" + inner + "}"
+    elif isinstance(obj, list):
+        return "[" + ", ".join(serialize(x) for x in obj) + "]"
+    elif isinstance(obj, tuple):
+        if len(obj) == 1:
+            return "(" + serialize(obj[0]) + ",)"
+        return "(" + ", ".join(serialize(x) for x in obj) + ")"
+    elif isinstance(obj, set):
+        inner = ", ".join(serialize(x) for x in sorted(obj, key=lambda x: serialize(x)))
+        return "{" + inner + "}"
+    elif isinstance(obj, str):
+        return f'"{obj}"'
+    else:
+        return str(obj)
+
+
+def deep_sort_to_string(obj):
+    sorted_obj = deep_sorted(obj)
+    return serialize(sorted_obj)
 
 if __name__ == '__main__':
-    x=eval(input()) 
+    x=eval(input())
     print(deep_sorted(x))
     import doctest
-    print(doctest.testmod())
+    print (doctest.testmod())
